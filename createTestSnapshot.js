a08a1e4969a39e9eed55c22a3fc8ad69e0a06ce1
Ludovic_Domingues <ludovic.domingues96@gmail.com>
2024-01-15 14:28:35 +0100

    added snapshot

--- createTestSnapshot.js
+++ createTestSnapshot.js
@@ -1,6 +1,6 @@
 async function fetchFromGraphQlTest() {
-    const url = 'https://hub.snapshot.org/graphql';
-    const query = `{
+  const url = 'https://hub.snapshot.org/graphql';
+  const query = `{
         proposal(id: "QmWbpCtwdLzxuLKnMW4Vv4MPFd2pdPX71YBKPasfZxqLUS") {
           id
           title
@@ -10,20 +10,20 @@ async function fetchFromGraphQlTest() {
           end
         }
       }`;
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query })
-    };
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query })
+  };
 
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
+  const response = await fetch(url, options);
+  const json = await response.json();
+  console.log(json);
 }
 
 async function getAllProposalsFromTest() {
-    const url = 'https://testnet.snapshot.org/#/geraldinehenry.eth';
-    const query = `{
+  const url = 'https://testnet.snapshot.org/#/geraldinehenry.eth';
+  const query = `{
         proposals (
           first: 20,
           skip: 0,
@@ -52,20 +52,20 @@ async function getAllProposalsFromTest() {
             name
           }
         }`;
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query })
-    };
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query })
+  };
 
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
+  const response = await fetch(url, options);
+  const json = await response.json();
+  console.log(json);
 }
 
 async function testQueryGeraldineSpace() {
-    const url = 'https://testnet.hub.snapshot.org/graphql';
-    const query = `query {
+  const url = 'https://testnet.hub.snapshot.org/graphql';
+  const query = `query {
         space(id: "geraldinehenry.eth") {
           id
           name
@@ -75,63 +75,53 @@ async function testQueryGeraldineSpace() {
           members
         }
       }`;
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query })
-    };
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query })
+  };
 
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
+  const response = await fetch(url, options);
+  const json = await response.json();
+  console.log(json);
 }
 
 async function testQueryGeraldineProposals() {
-    const url = 'https://testnet.hub.snapshot.org/graphql';
-    //const url = 'https://hub.snapshot.org/graphql';
-    const query = `query {
-        proposals (
-          first: 20,
-          skip: 0,
-          where: {
-            space_in: ["geraldinehenry.eth"],
-            state: "active"
-          },
-          orderBy: "created",
-          orderDirection: desc
-        ) {
-          id
-          title
-          body
-          choices
-          start
-          end
-          snapshot
-          state
-          scores
-          quorum
-          votes
-          scores_by_strategy
-          scores_total
-          scores_updated
-          author
-          space {
-            id
-            name
-          }
-        }
-      }`;
-    const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query })
-    };
+  const url = 'https://testnet.hub.snapshot.org/graphql';
+  //const url = 'https://hub.snapshot.org/graphql';
+  const query = `query {
+    proposals(first: 20, skip: 0, where: {space_in: ["geraldinehenry.eth"], state: "closed"}, orderBy: "created", orderDirection: desc) {
+      id
+      title
+      body
+      choices
+      start
+      end
+      snapshot
+      state
+      scores
+      scores_by_strategy
+      scores_total
+      scores_updated
+      votes
+      author
+      space {
+        id
+        name
+      }
+    }
+  }`;
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query })
+  };
 
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-    let proposals = json.data.proposals;
-    console.log(proposals);
+  const response = await fetch(url, options);
+  const json = await response.json();
+  console.log(json);
+  let proposals = json.data.proposals;
+  console.log(proposals);
 }
 
 
---
+++ proposalPage.js
@@ -0,0 +1,97 @@
+//i get the proposal component and hide it
+var component = document.getElementById("proposal");
+component.style.display = "none";
+
+var proposalList = document.getElementById("proposals-list");
+
+//technicallt proposalsData[i] is the same as proposals[i]
+let proposalsData = [];
+let proposals = [];
+
+async function testQueryGeraldineProposals() {
+    const url = 'https://testnet.hub.snapshot.org/graphql';
+    //const url = 'https://hub.snapshot.org/graphql';
+    const query = `query {
+      proposals(first: 20, skip: 0, where: {space_in: ["geraldinehenry.eth"], state: "closed"}, orderBy: "created", orderDirection: desc) {
+        id
+        title
+        body
+        choices
+        start
+        end
+        snapshot
+        state
+        scores
+        scores_by_strategy
+        scores_total
+        scores_updated
+        votes
+        author
+        space {
+          id
+          name
+        }
+      }
+    }`;
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query })
+    };
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+    return json.data.proposals;
+}
+
+async function main() {
+    proposalsData = await testQueryGeraldineProposals();
+
+    for (let i = 0; i < proposalsData.length; i++) {
+        var duplicatedComponent = component.cloneNode(true);
+        if (duplicatedComponent) {
+            duplicatedComponent.id = duplicatedComponent.id + i;
+
+            var children = duplicatedComponent.querySelectorAll('[id]');
+            children.forEach(function (child) {
+                switch (child.id) {
+                    case "proposal-titre":
+                        child.innerText = proposalsData[i].title;
+                        break;
+                    case "proposal-details":
+                        child.innerText = proposalsData[i].body;
+                        break;
+                    case "proposal-auteur":
+                        child.innerText = proposalsData[i].author;
+                        break;
+                    case "proposal-date":
+                        child.innerText = new Date(proposalsData[i].start*1000);
+                        break;
+                    case "proposal-statut":
+                        child.innerText = proposalsData[i].state;
+                        break;
+                }
+
+                child.id = child.id + i;
+            });
+
+            console.log(duplicatedComponent);
+
+            proposalList.appendChild(duplicatedComponent);
+            proposals.push(duplicatedComponent);
+        }
+    }
+
+    for (let i = 0; i < proposals.length; i++) {
+        proposals[i].style.display = "block";
+    }
+}
+
+function filterProposalsByStatus(status) {
+    const filteredProposals = proposalsData.filter(proposal => proposal.state === status);
+    return filteredProposals;
+}
+
+
+main();
