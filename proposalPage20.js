0cd9c40a448e8bba5c6c3cee2f4b1a332d47dc7a
Ludovic_Domingues <ludovic.domingues96@gmail.com>
2024-01-26 15:35:15 +0100

    a

--- proposalPage20.js
+++ proposalPage20.js
@@ -120,7 +120,7 @@ async function getLastDiscourseComments(proposalData) {
 
     commentAuthorName.innerText = postsData[i].username;
     commentDate.innerText = getTimeSincePost(postsData[i].created_at);
-    commentContent.innerText = postsData[i].cooked;
+    commentContent.html = postsData[i].cooked;
   }
 
   for (let i = numberOfComments; i < numberOfCommentsLimit; i++) {
@@ -129,7 +129,7 @@ async function getLastDiscourseComments(proposalData) {
   }
 }
 
-async function getTimeSincePost(created_at) {
+function getTimeSincePost(created_at) {
   const postDate = new Date(created_at);
   const now = new Date();
   const timeSincePost = now - postDate;
