47bbc968e36aeb91d10d77126e9163cb0e5831ea
Ludovic_Domingues <ludovic.domingues96@gmail.com>
2024-01-31 14:08:38 +0100

    a

--- proposalPage/proposalPage39.js
+++ proposalPage/proposalPage39.js
@@ -41,8 +41,9 @@ function setValuesFromProposalData(proposalData) {
   //category.innerHTML = proposalData.space.name;
 
   console.log(proposalData.state);
-  console.log(sipStatus.textContent);
-  sipStatus.textContent = proposalData.state;
+  console.log(sipStatus.innerText);
+  sipStatus.innerText = proposalData.state;
+  console.log(sipStatus.innerText);
 
   author.innerText = proposalData.author;
   endDate.innerText = new Date(proposalData.end * 1000);
