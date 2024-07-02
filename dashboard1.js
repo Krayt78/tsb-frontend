b5bc6c33adafd40d74aa8ecefa5f982c42df4ce4
Ludovic_Domingues <ludovic.domingues96@gmail.com>
2024-07-02 09:53:43 +0200

    started percentage remaining

--- dashboard1.js
+++ dashboard1.js
@@ -3,6 +3,7 @@ const currentBalanceElement = document.getElementById("budget-left");
 const sandInitiativesElements = {
     "sandRemaining": document.getElementById("sand-left-1"),
     "percentageRemaining": document.getElementById("percent-jauge-1"),
+    "fillingBar": document.getElementById("filling-bar-1"),
     "sandRemainingSmall": document.getElementById("sand-left-small-1"),
     "viewTransactions": document.getElementById("transaction-btn-1"),
 };
@@ -10,6 +11,7 @@ const sandInitiativesElements = {
 const liveOpsElements = {
     "sandRemaining": document.getElementById("sand-left-2"),
     "percentageRemaining": document.getElementById("percent-jauge-2"),
+    "fillingBar": document.getElementById("filling-bar-2"),
     "sandRemainingSmall": document.getElementById("sand-left-small-2"),
     "viewTransactions": document.getElementById("transaction-btn-2"),
 };
@@ -17,6 +19,7 @@ const liveOpsElements = {
 const gameContentElements = {
     "sandRemaining": document.getElementById("sand-left-3"),
     "percentageRemaining": document.getElementById("percent-jauge-3"),
+    "fillingBar": document.getElementById("filling-bar-3"),
     "sandRemainingSmall": document.getElementById("sand-left-small-3"),
     "viewTransactions": document.getElementById("transaction-btn-3"),
 };
@@ -24,6 +27,7 @@ const gameContentElements = {
 const otherElements = {
     "sandRemaining": document.getElementById("sand-left-4"),
     "percentageRemaining": document.getElementById("percent-jauge-4"),
+    "fillingBar": document.getElementById("filling-bar-4"),
     "sandRemainingSmall": document.getElementById("sand-left-small-4"),
     "viewTransactions": document.getElementById("transaction-btn-4"),
 };
@@ -31,6 +35,7 @@ const otherElements = {
 const operationsElements = {
     "sandRemaining": document.getElementById("sand-left-5"),
     "percentageRemaining": document.getElementById("percent-jauge-5"),
+    "fillingBar": document.getElementById("filling-bar-5"),
     "sandRemainingSmall": document.getElementById("sand-left-small-5"),
     "viewTransactions": document.getElementById("transaction-btn-5"),
 };
@@ -38,6 +43,7 @@ const operationsElements = {
 const stakingElements = {
     "sandRemaining": document.getElementById("sand-left-6"),
     "percentageRemaining": document.getElementById("percent-jauge-6"),
+    "fillingBar": document.getElementById("filling-bar-6"),
     "sandRemainingSmall": document.getElementById("sand-left-small-6"),
     "viewTransactions": document.getElementById("transaction-btn-6"),
 };
@@ -45,6 +51,7 @@ const stakingElements = {
 const sandboxForGoodElements = {
     "sandRemaining": document.getElementById("sand-left-7"),
     "percentageRemaining": document.getElementById("percent-jauge-7"),
+    "fillingBar": document.getElementById("filling-bar-7"),
     "sandRemainingSmall": document.getElementById("sand-left-small-7"),
     "viewTransactions": document.getElementById("transaction-btn-7"),
 };
@@ -52,6 +59,7 @@ const sandboxForGoodElements = {
 const platformElements = {
     "sandRemaining": document.getElementById("sand-left-8"),
     "percentageRemaining": document.getElementById("percent-jauge-8"),
+    "fillingBar": document.getElementById("filling-bar-8"),
     "sandRemainingSmall": document.getElementById("sand-left-small-8"),
     "viewTransactions": document.getElementById("transaction-btn-8"),
 };
@@ -59,6 +67,7 @@ const platformElements = {
 const nftElements = {
     "sandRemaining": document.getElementById("sand-left-9"),
     "percentageRemaining": document.getElementById("percent-jauge-9"),
+    "fillingBar": document.getElementById("filling-bar-9"),
     "sandRemainingSmall": document.getElementById("sand-left-small-9"),
     "viewTransactions": document.getElementById("transaction-btn-9"),
 };
@@ -67,39 +76,48 @@ let walletBalances =
 {
     "SAND Initiatives": {
         "address": "0xaE6e4117A3393F084baEa521a3AACB0a6D31Eac2",
-        "balance": "10.0"
+        "balance": "10.0",
+        "annualBudget": "3600000"
     },
     "Liveops": {
         "address": "0xe4bA55A399976f361685cB562812C726333a6008",
-        "balance": "20.0"
+        "balance": "20.0",
+        "annualBudget": "3000000"
     },
     "Game Content": {
         "address": "0x2994748F12CBbAA5A66f0833BdDA5166C1835394",
-        "balance": "30.0"
+        "balance": "30.0",
+        "annualBudget": "2500000"
     },
     "Other": {
         "address": "0x997FB6d902cAD0b7dD736829FB7871A8f28865f5",
-        "balance": "40.0"
+        "balance": "40.0",
+        "annualBudget": "2400000"
     },
     "Operations": {
         "address": "0xF7aEa344B416cB5D4F23e6310DAf4b14A81aE18A",
-        "balance": "50.0"
+        "balance": "50.0",
+        "annualBudget": "1800000"
     },
     "Staking": {
         "address": "0xF3E985704240211e4B6B1b3b4fc7901834263481",
-        "balance": "60.0"
+        "balance": "60.0",
+        "annualBudget": "900000"
     },
     "Sandbox for Good": {
         "address": "0x1B1C4163Cc89049c5b276A98202e0995444A2f00",
-        "balance": "70.0"
+        "balance": "70.0",
+        "annualBudget": "600000"
     },
     "Platform": {
         "address": "0x355C5263D09bb538983E3Fd384D42A2D070140d1",
-        "balance": "80.0"
+        "balance": "80.0",
+        "annualBudget": "600000"
     },
     "NFT": {
         "address": "0x0f80Afc7D2590F874c45b5142ec4463F5c413469",
-        "balance": "90.0"
+        "balance": "90.0",
+        "annualBudget": "100000"
     }
 };
 
@@ -141,18 +159,32 @@ function setSandRemainingElement(walletBalances) {
 }
 
 function setPercentageRemainingElement(walletBalances) {
+    sandInitiativesElements.percentageRemaining.innerHTML = (walletBalances["SAND Initiatives"].balance / walletBalances["SAND Initiatives"].annualBudget) * 100 + "%";
+    const percentage = (walletBalances["SAND Initiatives"].balance / walletBalances["SAND Initiatives"].annualBudget) * 100;
+    sandInitiativesElements.fillingBar.style.width = percentage + "%";
+    setColorOfFillinBar(sandInitiativesElements.fillingBar, percentage);
+}
+
+function setColorOfFillinBar(fillingBar, percentage) {
+    if (percentage < 20) {
+        fillingBar.style.backgroundColor = "#FF2545";
+    } else if (percentage < 60) {
+        fillingBar.style.backgroundColor = "#FFBF36";
+    } else {
+        fillingBar.style.backgroundColor = "#00CF74";
+    }
 }
 
 function setViewTransactionsElement(walletBalances) {
     sandInitiativesElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["SAND Initiatives"].address;
-    liveOpsElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Liveops"].address;
-    gameContentElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Game Content"].address;
-    otherElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Other"].address;
-    operationsElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Operations"].address;
-    stakingElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Staking"].address;
-    sandboxForGoodElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Sandbox for Good"].address;
-    platformElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Platform"].address;
-    nftElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["NFT"].address;
+    // liveOpsElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Liveops"].address;
+    // gameContentElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Game Content"].address;
+    // otherElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Other"].address;
+    // operationsElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Operations"].address;
+    // stakingElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Staking"].address;
+    // sandboxForGoodElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Sandbox for Good"].address;
+    // platformElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["Platform"].address;
+    // nftElements.viewTransactions.href = "https://polygonscan.com/address/" + walletBalances["NFT"].address;
 }
 
 function setElementsAccordingToBalances(walletBalances) {
